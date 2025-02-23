6657d296561b6dd24c4dc89f32cc8ebca033c5e3
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 15:56:20 +0300

    Unify plugins build

--- Plugin.NuGetPluginProvider/Properties/AssemblyInfo.cs
+++ Plugin.NuGetPluginProvider/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: Guid("25ea8c1f-932d-48c2-be4c-34e165f068d7")]
-[assembly: ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
 
 #if NETSTANDARD || NETCOREAPP
@@ -10,13 +9,6 @@ using System.Runtime.InteropServices;
 #else
 [assembly: AssemblyTitle("Plugin.NuGetPluginProvider")]
 [assembly: AssemblyProduct("NuGet plugin provider")]
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2019-2024")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2019-2025")]
 
 #endif
